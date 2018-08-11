fa2ae6aa483e878da7d246ab2248a42209712ea6
Ky-Anh Huynh <kyanh@viettug.org>
2018-08-12 04:29:58 +0700

    _utils/update-progress: clean fonts information
    Need to set height=16px in <img> tag

--- _utils/update_progress.sh
+++ _utils/update_progress.sh
@@ -18,6 +18,7 @@ cd "$_START_DIR" || exit
 echo "<progress max=\"100\" value=\"$_PERCENT\"><strong>Progress: $_PERCENT% done.</strong></progress>" \
   > "progress.html"
 
-wkhtmltoimage progress.html progress.svg
-sed -i -e '/viewBox=/d' progress.svg
-sed -i -e 's/<svg width=.*/<svg/' progress.svg
+wkhtmltoimage  --width 100 --height 24 progress.html progress.svg
+#sed -i -e '/viewBox=/d' progress.svg
+#sed -i -e 's/<svg width=.*/<svg/' progress.svg
+sed -i -e 's/font-family="Bitstream Vera Sans".*//' progress.svg
